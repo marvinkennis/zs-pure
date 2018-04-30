831245ce38fc341f6e802afea6554019d19909a0
Marvin Kennis <marvin.kennis@googlemail.com>
2018-04-29 23:21:15 -0400

    Added todo / progress

---
+++ src/Components/TODO_CheckboxSet.js
@@ -0,0 +1,20 @@
+
+import React, { Component} from 'react';
+import { Icon } from 'react-icons-kit'
+import '../styles/scss/App.css';
+
+class CheckboxSet extends Component {
+// Initialize some props
+ constructor(props){
+     super(props);
+ }
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+
+export default CheckboxSet;
---
+++ src/Components/TODO_RadioButtonSet.1.js
@@ -0,0 +1,21 @@
+
+import React, { Component} from 'react';
+import { Icon } from 'react-icons-kit'
+import '../styles/scss/App.css';
+import {ic_file_download} from 'react-icons-kit/md/ic_file_download'
+
+class RadioButtonSet extends Component {
+// Initialize some props
+ constructor(props){
+     super(props);
+ }
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+
+export default RadioButtonSet;
