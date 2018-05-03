b82ede45fcfec9396796914ce79bcf6caca47250
Marvin Kennis <marvin.kennis@googlemail.com>
2018-05-02 21:58:05 -0400

    Added progress indicator

--- src/Components/ProgressTracker.js
+++ src/Components/ProgressTracker.js
@@ -2,9 +2,10 @@
 import React, { Component} from 'react';
 import { Icon } from 'react-icons-kit'
 import '../styles/scss/App.css';
-import {ic_file_download} from 'react-icons-kit/md/ic_file_download'
+import '../styles/css/RadioButtonSet.css';
 
-class ProgressTracker extends Component {
+
+class ProgressTrackerVV extends Component {
 // Initialize some props
  constructor(props){
      super(props);
@@ -12,27 +13,38 @@ class ProgressTracker extends Component {
 
   render() {
     return (
-          <div>
-                    <ul class="progress vertical">
-          <li data-step ="1" class="active">Step 1</li>
-          <li data-step ="2">Step 2</li>
-          <li data-step ="3">Step 3</li>
-        </ul>
-          <div class="zs-progress-tracker">
-
-          <ul>
-            <li><input type="checkbox" disabled="disabled" class="progress-bulb"/>About</li>
-            <li><input type="checkbox" disabled="disabled" class="progress-bulb"/>Support</li>
-            <li><input type="checkbox" disabled="disabled" class="progress-bulb"/>Contact</li>
-          </ul>
-          </div>
-          </div>
-
+      <div>
+      <div class="container">
+    
+      <ul>
+      <li>
+      <div class="next-bridge filled"></div>
+        <input type="radio" id="f-option"  checked="checked" class="complete-option" disabled="disabled" name="selector1"/>
+        <label for="f-option">Label 1</label>
+        <div class="check-complete"></div>
+      </li>
+      
+      <li>
+      <div class="next-bridge"></div>
+        <input type="radio" id="s-option" checked="checked" disabled="disabled" name="selector2"/>
+        <label for="s-option">Label 2</label>
+        
+        <div class="check"><div class="inside"></div></div>
+      
+      </li>
+      
+      <li>
+        <input type="radio" id="t-option" disabled="disabled"  name="selector"/>
+        <label for="t-option">Label 3</label>
+        
+        <div class="check"><div class="inside"></div></div>
+      </li>
+    </ul>
+    </div>
+    </div>
     );
   }
 }
 
 
-
-
-export default ProgressTracker;
+export default ProgressTrackerVV;
